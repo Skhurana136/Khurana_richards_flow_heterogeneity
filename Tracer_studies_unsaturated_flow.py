10108728b1a483bcd7ac92a0b756253708014fed
Skhurana136 <swamini.khurana@gmail.com>
2022-08-03 18:39:29 +0200

    Updated time step of slow flow regime output to 2 days from 1 day to correct errors

--- Tracer_studies_unsaturated_flow.py
+++ Tracer_studies_unsaturated_flow.py
@@ -37,8 +37,8 @@ vedge = 0.0025
 velem = 0.005
 vbc = 0.3
 por = 0.2
-Regimes = ["Medium", "Fast", "Slow"]
-steps = [0.1,  0.01, 1.]
+Regimes = ["Slow", "Medium", "Fast"]
+steps = [2., 0.1,  0.01]
 droplist = []
 
 Trial = list(t for t in mTrial if t not in droplist)
@@ -48,22 +48,22 @@ for Reg, s in zip(Regimes, steps):
     d = os.path.join(raw_dir, Reg+"AR_0")
     filename = Reg+"AR_0_RF-AH_df.npy"
     data = np.load(os.path.join(d, filename))
-    conctime, TotalFlow, Headinlettime = ta.conc_time(data, yin, yout, xleft, xright, vertnodes, gvarnames, "Unsaturated")
-    Time = np.where(np.round(conctime[:, yout, 0], 3) > 10)
+    conctime, TotalFlow, Headinlettime = ta.conc_time(data, 0, -1, xleft, xright, vertnodes, gvarnames, "Unsaturated", element_length = velem, edge_length = vedge)
+    Time =  np.where(np.round(conctime[:, yout, 0], 3) > 10)
     initial = s*Time[0][0]
     for t in Trial:
         filename = Reg+"AR_0_RF-A"+t+"_df.npy"
         data = np.load(os.path.join(d, filename))
-        conctime, TotalFlow, Headinlettime = ta.conc_time(data, yin, yout, xleft, xright, vertnodes, gvarnames, "Unsaturated")
+        conctime, TotalFlow, Headinlettime = ta.conc_time(data, 0, -1, xleft, xright, vertnodes, gvarnames, "Unsaturated", element_length = velem, edge_length = vedge)
         Time = np.where(np.round(conctime[:, yout, 0], 3) > 10)
         print(s * Time[0][0], initial, (s * Time[0][0]) / initial)
         breakthrough2.append([Reg, t, scdict[t]['Het'], scdict[t]['Anis'], "Tracer", s*Time[0][0], (s*Time[0][0])/initial])
 
 data = pd.DataFrame(breakthrough2, columns = ["Regime", "Trial", "Variance", "Anisotropy", "Chem", "Time", "fraction"])
-f = os.path.join(output_dir, "tracer_03082022.csv")#11062021.csv")
+f = os.path.join(output_dir, "tracer_wo_effsat_03082022.csv")#11062021.csv")
 data.to_csv(f, index = 'False')
         
-# plotting boxplots to see variance of breakthrough from homogeneous scenario
+## plotting boxplots to see variance of breakthrough from homogeneous scenario
 tracerplot = gp.plot_tracer(f)
-plot_path = os.path.join(output_dir, "tracer_breakthrough_impact_03082022.png")#"tracer_breakthrough_impact.png")
+plot_path = os.path.join(output_dir, "tracer_breakthrough_impact_wo_effsat_03082022.png")#"tracer_breakthrough_impact.png")
 tracerplot.savefig(plot_path, dpi = 300, pad_inches = 0.1, bbox_inches = 'tight')
