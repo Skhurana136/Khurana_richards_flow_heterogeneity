cc786b9774fc7c66aed5aabc0b797f2830fa7efd
Skhurana136 <swamini.khurana@gmail.com>
2022-08-09 15:56:42 +0200

    Updated files generated and renamed diffusion to aeration

--- diffusion_calculation.py
+++ diffusion_calculation.py
@@ -13,25 +13,22 @@ print ("All libraries loaded")
 # Assign directories based on where you are
 # work computer
 data_dir = r"C:\Users\swkh9804\OneDrive\Documents\Manuscripts\Paper3\Figurecodes"
-raw_dir = r"D:\Data\Richards_flow\Richards_flow_big_sat"#D:\Data\Richards_flow\RF_big_sat_2"
-
+raw_dir = r"D:\Data\Richards_flow\RF_big_sat_2"
 # home computer
 #data_dir = r"C:\Users\swami\OneDrive\Documents\Manuscripts\Paper3\Figurecodes"
 #raw_dir = r"E:\Data\Richards_flow\Richards_flow_big_sat"#D:\Data\Richards_flow\RF_big_sat_2"
 # %%
 #Assign file names
-biomass_file = "biomass_comparison_with_sat_26092021.csv"
-chem_file = "conc_comparison_with_sat_26092021.csv"
+biomass_file = "biomass_comparison_09082022.csv"
+chem_file = "conc_comparison_09082022.csv"
 print ("Path directories and files assigned")
 
-filename = "concdata_with_sat_26092021.csv"#concdata_03082021.csv"
+filename = "concdata_09082022.csv"
 mfdata = pd.read_csv(os.path.join(data_dir, filename))
 print(mfdata.shape)
 print(mfdata.columns)
-filename = "conc_comparison_with_sat_26092021.csv"#03082021.csv"
+filename = "conc_comparison_09082022.csv"
 mfdata_comparison = pd.read_csv(os.path.join(data_dir, filename))
-#filename = "E:/Richards_flow/RF_big_sat_2/Da_unsaturated.csv"
-#mfdata_comparison = pd.read_csv(filename)
 print(mfdata_comparison.shape)
 print(mfdata_comparison.columns)
 
@@ -77,7 +74,8 @@ def Convertetoarray(D, datatype, vertnodes, horiznodes):
 
 allrates = proc.masterrates("Unsaturated")
 allrates
-
+yin = 6
+yout = -7
 aerorates = ['Fixedaeroresp', 'Mobaeroresp','Fixedammresp','Mobammresp','DOdiffusion']
 ratesind = list(19+allrates.index(a) for a in aerorates)
 
@@ -94,55 +92,50 @@ for r in Regimes:
         print(r, t)
         path = os.path.join(reg_dir, "RF-A"+str(t),"ratesAtFinish.dat")
         rates = np.loadtxt(path, usecols = ratesind, delimiter = ' ')
+        data_path = os.path.join(raw_dir, r + "AR_0" + "_RF-A"+str(t) + "_df.npy")
+        data = np.load(data_path)
+        sat = data[4,-1,:,:]
         rates_array = Convertetoarray(rates, "rates", 113, 61)
         resp_sum = 0
         resp_corners = 0
         resp_boundary_top = 0
-        resp_boundary_bot = 0
+        resp_boundary_side = 0
         resp_elem = 0
         diff_i = aerorates.index('DOdiffusion')
-        diff_corners = [rates_array[diff_i,6,0],rates_array[diff_i,6,-1],rates_array[diff_i,-7,0],rates_array[diff_i,-7,-1]]
-        diff_boundary_top = [rates_array[diff_i,6,1:-1],rates_array[diff_i,-7,1:-1]]
-        diff_boundary_bot = [rates_array[diff_i,7:-7,0],rates_array[diff_i,7:-7,-1]]
-        diff_elem = rates_array[diff_i,7:-7,1:-1]
-        diff_total = np.sum(diff_corners)*0.0025*0.0025+(np.sum(diff_boundary_top)+np.sum(diff_boundary_bot))*0.0025*0.005+np.sum(diff_elem)*0.005*0.005
+        diff_corners = [rates_array[diff_i,yin,0],rates_array[diff_i,yin,-1],rates_array[diff_i,yout,0],rates_array[diff_i,yout,-1]]
+        diff_boundary_top = [rates_array[diff_i,yin,1:-1],rates_array[diff_i,yout,1:-1]]
+        diff_boundary_side = [rates_array[diff_i,yin+1:yout,0],rates_array[diff_i,yin+1:yout,-1]]
+        diff_elem = rates_array[diff_i,yin+1:yout,1:-1]
+        sat_corners = np.asarray([sat[yin,0],sat[yin,-1],sat[yout,0],sat[yout,-1]])
+        sat_boundary_top = np.asarray([sat[yin,1:-1],sat[yout,1:-1]])
+        sat_boundary_side = np.asarray([sat[yin+1:yout,0],sat[yin+1:yout,-1]])
+        sat_elem = sat[yin+1:yout,1:-1]
+        diff_total = np.sum(diff_corners*sat_corners)*0.0025*0.0025+(np.sum(diff_boundary_top*sat_boundary_top)+np.sum(diff_boundary_side*sat_boundary_side))*0.0025*0.005+np.sum(diff_elem*sat_elem)*0.005*0.005
+        row.append([r,t,"DO",'aeration',diff_total])
         for r_i in list(range(len(aerorates)-1)):
-            corners = np.asarray([rates_array[r_i,6,0],rates_array[r_i,6,-1],rates_array[r_i,-7,0],rates_array[r_i,-7,-1]])
-            boundary_top = np.asarray([rates_array[r_i,6,1:-1],rates_array[r_i,-7,1:-1]])
-            boundary_bot = np.asarray([rates_array[r_i,7:-7,0],rates_array[r_i,7:-7,-1]])
-            elem = rates_array[r_i,7:-7,1:-1]
-            totaldorem = np.sum(corners)*0.0025*0.0025+(np.sum(boundary_top)+np.sum(boundary_bot))*0.0025*0.005+np.sum(elem)*0.005*0.005
+            corners = np.asarray([rates_array[r_i,yin,0],rates_array[r_i,yin,-1],rates_array[r_i,yout,0],rates_array[r_i,yout,-1]])
+            boundary_top = np.asarray([rates_array[r_i,yin,1:-1],rates_array[r_i,yout,1:-1]])
+            boundary_side = np.asarray([rates_array[r_i,yin+1:yout,0],rates_array[r_i,yin+1:yout,-1]])
+            elem = rates_array[r_i,yin+1:yout,1:-1]
+            totaldorem = np.sum(corners*sat_corners)*0.0025*0.0025+(np.sum(boundary_top*sat_boundary_top)+np.sum(boundary_side*sat_boundary_side))*0.0025*0.005+np.sum(elem*sat_elem)*0.005*0.005
             resp_corners = resp_corners + corners
             resp_boundary_top = resp_boundary_top + boundary_top
-            resp_boundary_bot = resp_boundary_bot +  boundary_bot
+            resp_boundary_side = resp_boundary_side +  boundary_side
             resp_elem += elem
             resp_sum += totaldorem
             row.append([r,t,"DO",aerorates[r_i],totaldorem])
-        resp_diff_ratio_corners = resp_corners/diff_corners
-        resp_diff_ratio_boundary_top = resp_boundary_top/diff_boundary_top
-        resp_diff_ratio_boundary_bot = resp_boundary_bot/diff_boundary_bot
-        resp_diff_ratio_elem = resp_elem/diff_elem
-        resp_diff_ratio_arrays = [resp_diff_ratio_corners,resp_diff_ratio_boundary_top,resp_diff_ratio_boundary_bot,resp_diff_ratio_elem]
-        resp_diff_g_1_number = sum(list(np.size(np.argwhere(z.flatten()>1)) for z in resp_diff_ratio_arrays))
-        resp_diff_e_1_number = sum(list(np.size(np.argwhere(z.flatten()==1)) for z in resp_diff_ratio_arrays))
-        resp_diff_l_1_number = sum(list(np.size(np.argwhere(z.flatten()<1)) for z in resp_diff_ratio_arrays))
-        resp_diff_ratio_total = (resp_sum)/diff_total
-        row.append([r,t,"DO","Respiration_diffusion_ratio_total",resp_diff_ratio_total])
-        row.append([r,t,"DO","Respiration_diffusion_>1_num",resp_diff_g_1_number])
-        row.append([r,t,"DO","Respiration_diffusion_=1_num",resp_diff_e_1_number])
-        row.append([r,t,"DO","Respiration_diffusion_<1_num",resp_diff_l_1_number])
+        row.append([r,t,"DO","Total_respiration",resp_sum])
 netdorem = pd.DataFrame.from_records(row, columns = ["Regime", "Trial", "Chem","rate","rate_val"])
 
 sat_dorem = pd.merge(mfdata_comparison, netdorem, on = ["Regime", "Trial", "Chem"])
 
 sat_dorem.head()
 
-#for r in Regimes:
-#    base = sat_dorem[(sat_dorem['Regime']==r) & (sat_dorem['Trial']=="H")]["diffusion"].values[0]
-#    for t in Trial:
-#        sat_dorem.loc[(sat_dorem['Regime']==r) & (sat_dorem['Trial']==t),"diffusion_base"]=base
-#sat_dorem['diffusion_fraction'] = sat_dorem['diffusion']/sat_dorem['diffusion_base']
-#sat_dorem['diffusion_fraction%'] = sat_dorem['diffusion_fraction']*100
+for r in Regimes:
+    base = sat_dorem[(sat_dorem['Regime']==r) & (sat_dorem['Trial']=="H")]["aeration"].values[0]
+    for t in Trial:
+        sat_dorem.loc[(sat_dorem['Regime']==r) & (sat_dorem['Trial']==t),"aeration_base"]=base
+sat_dorem['aeration_fraction'] = sat_dorem['aeration']/sat_dorem['aeration_base']
+sat_dorem['aeration_fraction%'] = sat_dorem['aeration_fraction']*100
 
-sat_dorem.to_csv(os.path.join(data_dir,"aero_rates_het.csv"), index =False)
-# %%
+sat_dorem.to_csv(os.path.join(data_dir,"aero_rates_09082022.csv"), index =False)
