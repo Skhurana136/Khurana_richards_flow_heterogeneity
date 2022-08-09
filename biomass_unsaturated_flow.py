Updated indices for correct calculation for MS

biomass_unsaturated_flow.py
@@ -43,7 +43,7 @@ for Reg in Regimes:
         directory = parent_dir
         filename = Reg+"AR_0_RF-A"+str(j)+"_df.npy"
         data = np.load(os.path.join(directory, filename))
-        sat = np.mean(data[4,-1,6:-6,:])
+        sat = np.mean(data[4,-1,yin:yout+1,:])
         sumbio = ssa.sum_biomass(data, yin, yout, xleft, xright, gvarnames, "Unsaturated")
         total = sum(sumbio)
         for g in gvarnames:
@@ -60,7 +60,7 @@ tr_data.columns
 #Merge the datasets and save
 cdata = pd.merge(biomassdata, tr_data[["Trial", "Regime", "Time", "fraction"]], on = ["Regime", "Trial"])
 
-cdata.to_csv(os.path.join(results_dir,"biomass_with_sat_09082022.csv"), index=False)
+cdata.to_csv(os.path.join(results_dir,"biomass_09082022.csv"), index=False)
 
 row = []
 for Reg in Regimes:
@@ -68,7 +68,7 @@ for Reg in Regimes:
         directory =  parent_dir
         filename = Reg+"AR_0_RF-A"+str(j)+"_df.npy"
         data = np.load(os.path.join(directory, filename))
-        sat = np.mean(data[4,-1,6:-6,:])
+        sat = np.mean(data[4,-1,yin:yout+1,:])
         sumbio = ssa.sum_biomass(data, yin, yout, xleft, xright, gvarnames, "Unsaturated")
         total = sum(sumbio)
         for g in gvarnames:
@@ -85,4 +85,4 @@ tr_data.columns
 #Merge the datasets and save
 cdata = pd.merge(biomassdata, tr_data[["Trial", "Regime", "Time", "fraction"]], on = ["Regime", "Trial"])
 
-cdata.to_csv(os.path.join(results_dir,"biomass_with_sat_09082022.csv"), index=False)
+cdata.to_csv(os.path.join(results_dir,"biomass_09082022.csv"), index=False)

mass_flux_unsaturated_flow.py
@@ -43,7 +43,7 @@ for Reg in Regimes:
         directory =  os.path.join(parent_dir)#, Reg + "AR_0")
         filename = Reg+"AR_0_RF-A"+str(j)+"_df.npy"
         data = np.load(os.path.join(directory, filename))
-        sat = np.mean(data[4,-1,yin:yout,:])
+        sat = np.mean(data[4,-1,yin:yout+1,:])
         massfluxin, massfluxout = ssa.massflux(data, yin, yout, xleft, xright, gvarnames, "Unsaturated", vedge, velem, por)
         delmassflux = -1*(massfluxin - massfluxout)
         reldelmassflux = 100*delmassflux/massfluxin
@@ -70,7 +70,7 @@ for Reg in Regimes:
         directory =  os.path.join(parent_dir)#, Reg + "AR_0")
         filename = Reg+"AR_0_RF-A"+str(j)+"_df.npy"
         data = np.load(os.path.join(directory, filename))
-        sat = np.mean(data[4,-1,yin:yout,:])
+        sat = np.mean(data[4,-1,yin:yout+1,:])
         conctime, TotalFlow, Headinlettime = sta.conc_time(data, yin, yout, xleft, xright, vertnodes, gvarnames, "Unsaturated")
         delconc = conctime[-1, yin, :] - conctime[-1,yout,:]
         reldelconc = 100*delconc/conctime[-1,yin,:]

massfluxdata_steady_state_comparison.py
@@ -8,9 +8,9 @@ import os
 import pandas as pd
 import numpy as np
 
-results_dir = "Y:/Home/khurana/4. Publications/Paper3/Figurecodes"
+results_dir = r"C:\Users\swkh9804\OneDrive\Documents\Manuscripts\Paper3\Figurecodes"
 #Load data
-path_data = os.path.join(results_dir, "massflux_with_sat_26092021.csv")
+path_data = os.path.join(results_dir, "massflux_09082022.csv")
 data = pd.read_csv(path_data)
 data.columns
 data.dtypes
@@ -42,11 +42,11 @@ data['normmassflux_spatial_fraction'] = data['normmassflux']/data['spatial_normm
 data['reldelmassflux_spatial_fraction'] = data['reldelmassflux']/data['spatial_reldelmassflux_base']
 data["Da63"] = np.log10(data.normmassflux)
 
-data.to_csv(os.path.join(results_dir,"massflux_comparison_with_sat_26092021.csv"),
+data.to_csv(os.path.join(results_dir,"massflux_comparison_09082022.csv"),
             index = False)
 
 #Load data
-path_data = os.path.join(results_dir,"concdata_with_sat_26092021.csv")
+path_data = os.path.join(results_dir,"concdata_09082022.csv")
 data = pd.read_csv(path_data)
 data.columns
 data.dtypes
@@ -78,4 +78,4 @@ data['normconc_spatial_fraction'] = data['normconc']/data['spatial_normconc_base
 data['reldelconc_spatial_fraction'] = data['reldelconc']/data['spatial_reldelconc_base']
 data["Da63"] = np.log10(data.normconc)
 
-data.to_csv(os.path.join(results_dir,"conc_comparison_with_sat_26092021.csv"), index = False)
+data.to_csv(os.path.join(results_dir,"conc_comparison_09082022.csv"), index = False)